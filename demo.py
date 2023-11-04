87fda3f557a4fcaacb4654b7e528ac1ffbf47320
blameitonme1 <2741122036@qq.com>
2023-11-04 22:52:09 +0800

    事实证明，换一个库就行了，选了一个telegram-python-bot库，这种基本的操作直接看官方文档即可，结果网络问题就正常了，选择大于努力我只能说

--- demo.py
+++ demo.py
@@ -1,3 +1,13 @@
-import telepot
-telegramBot = telepot.Bot("6471157961:AAGEUv0WAY8XsU7WsgwZC4Db3ZRp4K7P-Ds")
-telegramBot.getMe()
+import asyncio
+import telegram
+
+
+async def main():
+    bot = telegram.Bot("6471157961:AAGEUv0WAY8XsU7WsgwZC4Db3ZRp4K7P-Ds")
+    async with bot:
+        print(await bot.get_me())
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
